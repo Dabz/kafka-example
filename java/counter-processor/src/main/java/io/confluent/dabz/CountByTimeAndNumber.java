22e58704fca24ef02ef1094705423ca58ac53a62
Damien Gasparina <d.gasparina@gmail.com>
2019-01-07 14:25:51 +0100

    adding counter exemple

---
+++ java/counter-processor/src/main/java/io/confluent/dabz/CountByTimeAndNumber.java
@@ -0,0 +1,66 @@
+package io.confluent.dabz;
+
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.processor.Processor;
+import org.apache.kafka.streams.processor.ProcessorContext;
+import org.apache.kafka.streams.processor.PunctuationType;
+import org.apache.kafka.streams.processor.StateStore;
+import org.apache.kafka.streams.state.KeyValueIterator;
+import org.apache.kafka.streams.state.KeyValueStore;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class CountByTimeAndNumber implements Processor<String, String> {
+    private ProcessorContext context;
+    private KeyValueStore<String, String> kvStore;
+
+    private HashSet<String> dirtyQueue = new HashSet<String>();
+
+    public static final String STATE_STORE_NAME = "count-store";
+    public static final Integer MAX_NUMBER_OF_MESSAGES = 5;
+    public Integer numberOfMessage = 0;
+
+    public void init(ProcessorContext processorContext) {
+        this.context = processorContext;
+        this.kvStore = (KeyValueStore) processorContext.getStateStore(STATE_STORE_NAME);
+
+        this.context.schedule(30000, PunctuationType.WALL_CLOCK_TIME, (timestamp) -> {
+            flush();
+        });
+    }
+
+    public void process(String key, String value) {
+        if (key == null) {
+            key = "";
+        }
+        synchronized (this) {
+            kvStore.put(key, value);
+            numberOfMessage += 1;
+        }
+
+        if (numberOfMessage >= MAX_NUMBER_OF_MESSAGES) {
+            flush();
+        }
+    }
+
+    private void flush() {
+        synchronized (this) {
+            numberOfMessage = 0;
+            KeyValueIterator<String, String> keyValueIterator = this.kvStore.all();
+            while (keyValueIterator.hasNext()) {
+                KeyValue<String, String> keyValue = keyValueIterator.next();
+                context.forward(keyValue.key, keyValue.value);
+                this.kvStore.delete(keyValue.key);
+            }
+        }
+    }
+
+    public void punctuate(long l) {
+
+    }
+
+    public void close() {
+
+    }
+}
---
+++ java/counter-processor/src/main/java/io/confluent/dabz/Main.java
@@ -0,0 +1,42 @@
+package io.confluent.dabz;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.state.StoreBuilder;
+import org.apache.kafka.streams.state.Stores;
+
+import java.util.Properties;
+import java.util.UUID;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        properties.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG, 1);
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "count");
+
+        Topology builder = new Topology();
+
+        StoreBuilder<KeyValueStore<String, String>> aggregateStore = Stores.keyValueStoreBuilder(
+                Stores.persistentKeyValueStore(CountByTimeAndNumber.STATE_STORE_NAME),
+                Serdes.String(),
+                Serdes.String());
+
+        builder.addSource("source", "count-source")
+                .addProcessor("process", () -> new CountByTimeAndNumber(), "source")
+                .addStateStore(aggregateStore, "process")
+                .addSink("sink", "count-sink", "process");
+
+
+        KafkaStreams streams = new KafkaStreams(builder, properties);
+
+        streams.start();
+    }
+}
